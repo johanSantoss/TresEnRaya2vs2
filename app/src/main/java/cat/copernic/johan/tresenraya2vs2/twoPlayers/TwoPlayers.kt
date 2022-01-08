929c3dbdb1936a89c29ed1e9c7d2dcbde99e5e63
wasach <74709691+wasach@users.noreply.github.com>
2022-01-08 21:58:07 +0100

    + view model player2
    - faltan mods de player2

--- app/src/main/java/cat/copernic/johan/tresenraya2vs2/twoPlayers/TwoPlayers.kt
+++ app/src/main/java/cat/copernic/johan/tresenraya2vs2/twoPlayers/TwoPlayers.kt
@@ -1,26 +1,46 @@
 package cat.copernic.johan.tresenraya2vs2.twoPlayers
 
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import cat.copernic.johan.tresenraya2vs2.R
+import cat.copernic.johan.tresenraya2vs2.databinding.MenuFragmentBinding
+import cat.copernic.johan.tresenraya2vs2.databinding.TwoPlayersFragmentBinding
+import cat.copernic.johan.tresenraya2vs2.menu.MenuViewModel
 
 class TwoPlayers : Fragment() {
 
     companion object {
         fun newInstance() = TwoPlayers()
     }
-
+    private lateinit var binding: TwoPlayersFragmentBinding
     private lateinit var viewModel: TwoPlayersViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.two_players_fragment, container, false)
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.two_players_fragment,
+            container,
+            false
+        )
+        viewModel = ViewModelProvider(this).get(TwoPlayersViewModel::class.java)
+
+        binding.btnRestart2.setOnClickListener {
+            resetGame()
+        }
+
+
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -28,5 +48,21 @@ class TwoPlayers : Fragment() {
         viewModel = ViewModelProvider(this).get(TwoPlayersViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
+    private fun resetGame(){
+        // si es 0 - pasar a 1 y backg color yellow
+        // si es 1 - pasar a 2 y backg color red
+        // si es 2 - pasar a 0 y backg color btn reset / reset tablero
+
+    }
+    private fun resetTablero(){
+
+    }
+
+    private fun comprobarEstado(){
+        // comprobar estado boton reset
+
+    }
+
 
 }
--- app/src/main/java/cat/copernic/johan/tresenraya2vs2/twoPlayers/TwoPlayersViewModel.kt
+++ app/src/main/java/cat/copernic/johan/tresenraya2vs2/twoPlayers/TwoPlayersViewModel.kt
@@ -1,7 +1,82 @@
 package cat.copernic.johan.tresenraya2vs2.twoPlayers
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TwoPlayersViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    // jugador actual
+    private val _player = MutableLiveData(1)
+    val player: LiveData<Int> get() = _player
+
+    fun setPlayer (player : Int){
+        _player.value = player
+    }
+
+    // resetGame
+    private val _resetGame = MutableLiveData(0)
+    val resetGame: LiveData<Int> get() = _resetGame
+
+    fun upResetGame(){
+        _resetGame.value =+ 1
+    }
+    fun resetGame(){
+        _resetGame.value = 0
+    }
+
+//    // Partida en juego o no
+//    private val _game = MutableLiveData(0)
+//    val game: LiveData<Int> get() = _game
+//
+//    fun setGame(game : Int){
+//        _game.value = game
+//    }
+
+
+    // estado de la partida
+    // 1 finalizado // 0 en curso
+    private val _estadoPartida = MutableLiveData(0)
+    val estadoPartida: LiveData<Int> get() = _estadoPartida
+
+    fun setEstadoPartida (estado : Int){
+        _estadoPartida.value = estado
+    }
+
+    // total de Fichas jugadas
+    private val _totalFichas = MutableLiveData(0)
+    val totalFichas: LiveData<Int> get() = _totalFichas
+
+    fun upTotalFichas(){
+        _totalFichas.value =+ 1
+    }
+
+    // tablero
+    // 1- player1 // 2- player2
+    private val _tablero: MutableList<Int> = mutableListOf(   0,0,0,
+                                            0,0,0,
+                                            0,0,0)
+
+    val tablero : List<Int>  get() = _tablero
+    fun setTablero(index : Int , jugada : Int){
+        _tablero[index] = jugada
+    }
+
+    // botones
+    private val _botones: MutableList<Int> = mutableListOf( 0,0,0,
+                                                            0,0,0,
+                                                            0,0,0)
+
+    val botones : List<Int>  get() = _botones
+    fun setBotones(listaBotones : List<Int>){
+        _botones.clear()
+        _botones.addAll(listaBotones)
+    }
+
+
+
+
+
+
+
+
 }
